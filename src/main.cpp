7a29dbf4f7693a1f5464e2dfe832743b2e202580
apeiris <apeiris@hotmail.com>
2020-05-22 19:15:58 -0400

    added webthing

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <Arduino.h>
+#include <Print.h>
+#include <Thing.h>
+#include <WebThingAdapter.h>
+#include <WiFi.h>
+
+const char *ssid = "Peiris_Wifi";
+const char *wifiPassword = "22051954";
+
+IPAddress sqlIP(192,168,0,12);
+
+int loginCounter = 0;
+WebThingAdapter *adapter;
+String mac=WiFi.macAddress();
+
+void setup()
+{
 
 
+#if defined(ESP8266) || defined(ESP32)
+  WiFi.mode(WIFI_STA);
+#endif
 
-void setup() {
-  // put your setup code here, to run once:
-  int a=12,b=-1200;
-  printf("HI a=%i b=%i \n",a,b);
-  ESP_LOGD(TAG,"HIIIIII\n");
+  Serial.print("Connecting :\n ");
+  WiFi.begin(ssid, wifiPassword);
+mac.replace(":","");
+ printf("Connected... mac=%s\n",mac.c_str());
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
